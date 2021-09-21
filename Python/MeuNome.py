1dd3d43e5727b76e249e68337af2375bf147738f
Tais Lima <taisb_lima@usp.com.br>
2021-09-21 22:31:44 +0000

    adding MeuNome.py

---
+++ Python/MeuNome.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+print("Meu nome é Taís")
+print("Minha cor favorita é verde")
+
+atividadefav = "dormir"
+print("Minha atividade favorita é ", atividadefav)
+
+animalfav = "gato"
+print("Meu animal favorito é ", animalfav)
+
+print()
+import sys
+nome = sys.argv[1]
+corfav = sys.argv[2]
+atividadefav = sys.argv[3]
+animalfav = sys.argv[4]
+
+print('Meu nome é', nome, 'minha cor favorita é', corfav,'minha atividade favorita é',atividadefav,'e meu animal favorito é,' animalfav)
+
